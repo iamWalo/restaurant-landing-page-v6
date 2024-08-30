c033df0e71386617840618952310da1a9ec00c99
iamWalo <136198398+iamWalo@users.noreply.github.com>
2024-08-30 19:51:25 +0100

    Add files via upload

--- javaScript/script.js
+++ javaScript/script.js
@@ -3,7 +3,32 @@ window.addEventListener('load', () => {
     document.querySelector('.loader').style.display = 'none';
     document.querySelector('.content').style.display = 'block';
 });
+// side bar
 
+const menuOpen = document.querySelector('#menu-open');
+const menuClose = document.querySelector('#menu-close');
+const sidebar = document.querySelector('.sidebar');
+const sidebarButton = document.querySelector("div.header-button")
+const html = document.querySelector('html');
+menuOpen.addEventListener('click', () => {
+    sidebar.classList.add('show-sidebar');
+    sidebarButton.style.display = 'block';
+    sidebar.appendChild(sidebarButton);
+    html.style.overflow = 'hidden';
+    menuOpen.style.display = 'none';
+})
+menuClose.addEventListener('click', () => {
+    sidebarButton.style.display = 'none';
+    html.style.overflow = 'scroll';
+    sidebar.classList.remove('show-sidebar');
+    menuOpen.style.display = 'block';
+})
+document.addEventListener('click', function (event) {
+    const isClickInside = sidebar.contains(event.target) || menuOpen.contains(event.target);
+    if (!isClickInside) {
+        sidebar.classList.remove('show-sidebar');
+    }
+})
 // vedios animations
 const videos = document.querySelectorAll('.myVideo');
 videos.forEach(video => {
@@ -27,5 +52,4 @@ buttons.forEach((button) => {
         button.classList.add('clicked');
     })
 })
-
-
+// about Us slider 
